99d97ffd6a6df7b83edc2c331b6b56f3220015a0
Michael Jeffrey Wu <michaeljeffreywu@gmail.com>
2014-10-27 17:22:51 -0700

    Added the overlay to thumbnails

--- js/album.js
+++ js/album.js
@@ -24,10 +24,10 @@ app.service('productService', function ($window) {
 app.factory("serverService", function() {
     return {
         //dev
-        serverHost: 'localhost',//'162.242.170.162',
+        serverHost: '162.242.170.162',
         serverPort: '8081',
-        serverAuth: 'Basic dGVzdDphc2Rm',
-        // serverAuth: 'Basic YWdyaWdnczplcGljaG91c2U=',
+        // serverAuth: 'Basic dGVzdDphc2Rm',
+        serverAuth: 'Basic YWdyaWdnczplcGljaG91c2U=',
         //production
         //serverHost: '127.0.0.1',
         // serverPort: '8081'    
