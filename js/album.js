e62b8206f5f03ead0654e40742058a4e6520cfe2
Alex Griggs <alex@adventurebucketlist.com>
2014-10-31 14:02:55 -0700

    log data.token

--- js/album.js
+++ js/album.js
@@ -159,6 +159,7 @@ app.controller('PaymentCtrl', function ($scope, $http, $timeout, productService,
                 });
             });
         $.getJSON("http://"+serverService.serverHost+"/api/clientID", function (data) {
+            console.log(data.token);
             braintree.setup(data.token, "dropin", { container: "dropin"});
         });
                 // , "<integration>", options
