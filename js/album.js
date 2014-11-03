7e1ecd4bf5609e4fd8f9122bd2559a5d34a89703
Alex Griggs <alex@adventurebucketlist.com>
2014-11-03 13:55:27 -0800

    added formData log

--- js/album.js
+++ js/album.js
@@ -241,7 +241,7 @@ app.controller('PaymentCtrl', function ($scope, $http, $timeout, productService,
                 form.formData.number_of_children = $scope.numberOfChildren;
                 // var dateTime = new Date(form.formData.date+form.formData.time);
                 
-                // console.log($scope.timez, $scope.newtime);
+                console.log(form.formData);
                 form.formData.date_togo = form.formData.date; //new Date(form.formData.date+form.formData.time);
                 form.formData.time_togo = $scope.timez;//new Date(form.formData.time);
                 form.formData.api_key = location.search.split("api_key=")[1];
