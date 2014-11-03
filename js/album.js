84f8745f3e167741ff8cf8e1ab1d683504a50998
Alex Griggs <alex@adventurebucketlist.com>
2014-11-03 15:31:45 -0800

    console log to get rate

--- js/album.js
+++ js/album.js
@@ -30,9 +30,8 @@ app.factory("convertCurrency", function(){
         getCurrencyRate: function() {
             var query = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22"+currency.convertFrom+currency.convertTo+"%22)&format=json&env=store://datatables.org/alltableswithkeys&callback=";
              $.getJSON(query, function (data) {
-                console.log(data);
-                console.log(data.query.results);
-                currency.rate = data.results.rate.Rate;
+                console.log(data.query.results.rate.Rate);
+                currency.rate = data.query.results.rate.Rate;
              });
             return currency.rate; 
         },
