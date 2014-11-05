da6cd900bad5c55242bad6f1f0888984d6d2f67c
Alex Griggs <alex@adventurebucketlist.com>
2014-11-04 20:11:05 -0800

    trying to get nonce out of ajax call

--- js/album.js
+++ js/album.js
@@ -247,7 +247,7 @@ app.controller('PaymentCtrl', function ($scope, $http, $timeout, productService,
               cache: false,
               async: false
             });
-        
+        console.log("nonce to pass "+ NonceData.getNonce()+ $scope.nonce);
                 // , "<integration>", options
             // create a blank object to hold our form information
             // $scope will allow this to pass between controller and view
