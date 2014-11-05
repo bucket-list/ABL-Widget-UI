f93c7d5a1831cf06d97adbd3d8558c91db150579
Alex Griggs <alex@adventurebucketlist.com>
2014-11-04 16:58:38 -0800

    removed submit event

--- js/album.js
+++ js/album.js
@@ -329,7 +329,7 @@ app.directive('sumbitBT', function ($document) {
     var linkFn;
     linkFn = function(scope, element, attrs) {
         console.log(element);
-        element.getElementById('dropin').dispatchEvent(new Event('submit'));
+        //element.getElementById('dropin').dispatchEvent(new Event('submit'));
     };
     return {
         restrict: 'C',
