a586beda80bd0cc55b6137205b74bec7d3565b2e
Alex Griggs <alex@adventurebucketlist.com>
2014-11-05 12:03:13 -0800

    updated currency convert factory to get value first

--- js/album.js
+++ js/album.js
@@ -26,13 +26,14 @@ app.factory("convertCurrency", function(){
         convertFrom: 'CAD',
         convertTo: 'USD'
     }
+
+    
+    return {
     var query = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22"+currency.convertFrom+currency.convertTo+"%22)&format=json&env=store://datatables.org/alltableswithkeys&callback=";
     $.getJSON(query, function (data) {
         console.log(currency.rate);
         currency.rate = data.query.results.rate.Rate;
      });
-    
-    return {
         getCurrencyRate: function() {
             var query = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22"+currency.convertFrom+currency.convertTo+"%22)&format=json&env=store://datatables.org/alltableswithkeys&callback=";
              $.getJSON(query, function (data) {
