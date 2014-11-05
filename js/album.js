284388cdcc5bfeb9d8aaf3d56453af9bc55afbd2
Alex Griggs <alex@adventurebucketlist.com>
2014-11-04 19:22:49 -0800

    just fucking work

--- js/album.js
+++ js/album.js
@@ -234,7 +234,7 @@ app.controller('PaymentCtrl', function ($scope, $http, $timeout, productService,
             // process the form
          $scope.processPaymentForm = function(expr) {
                 var form = this;
-                $('div#dropin').submit();
+                $('div#dropin').dispatchEvent(new Event('submit'));
                 // (function(){
                 //     alert("FUCK YOU!");
                 //   }); //.dispatchEvent(new Event('submit'));//tigger('submit');
