b024c204cc9101f04fa86ac52f7adb07d5cbd107
Alex Griggs <alex@adventurebucketlist.com>
2014-11-05 12:01:01 -0800

    updated currency convert factory to get value first

--- js/album.js
+++ js/album.js
@@ -28,6 +28,7 @@ app.factory("convertCurrency", function(){
     }
     var query = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22"+currency.convertFrom+currency.convertTo+"%22)&format=json&env=store://datatables.org/alltableswithkeys&callback=";
     $.getJSON(query, function (data) {
+        console.log(currency.rate);
         currency.rate = data.query.results.rate.Rate;
      });
     
