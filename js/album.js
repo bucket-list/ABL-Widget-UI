de63c03941921f4af02d5e81512172e635919726
Alex Griggs <alex@adventurebucketlist.com>
2014-11-04 18:15:23 -0800

    process form broken with

--- js/album.js
+++ js/album.js
@@ -234,7 +234,7 @@ app.controller('PaymentCtrl', function ($scope, $http, $timeout, productService,
             // process the form
          $scope.processPaymentForm = function(expr) {
                 var form = this;
-                $("#dropin").dispatchEvent(new Event('submit'));
+                //$("#dropin").dispatchEvent(new Event('submit'));
                 $scope.selTime = $scope.timez[0];
                 form.formData.product_id = $scope.currentImage._id;
                 form.formData.subtotal = $scope.paymentSubtotal;
