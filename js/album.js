c0cfeaf183fe9753c2d092c74f33d699ea3496e0
Alex Griggs <alex@adventurebucketlist.com>
2014-11-05 15:55:14 -0800

    trying to round numbers

--- js/album.js
+++ js/album.js
@@ -521,6 +521,7 @@ app.controller('AlbumCtrl', function ($scope, $http, $timeout, $rootScope, produ
             };
           
           $scope.initSlider();
+          alert("USD: "+ ($scope.currentImage.price * $scope.rate) + " toFixed: "+ ($scope.currentImage.price * $scope.rate).toFixed(2))
     $scope.currentImage.price = ($scope.currentImage.price * $scope.rate).toFixed(2);
     // $scope.nextSlide = function(index) {
     //     var newindex = index + 1
