bd083ef24a61009103ef277d1c05f9bdd0e26460
Alex Griggs <alex@adventurebucketlist.com>
2014-11-05 12:11:37 -0800

    updated currency convert factory to get value first

--- js/album.js
+++ js/album.js
@@ -35,11 +35,11 @@ app.factory("convertCurrency", function(){
     });
     
     return {
-    var query = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22"+currency.convertFrom+currency.convertTo+"%22)&format=json&env=store://datatables.org/alltableswithkeys&callback=";
-    $.getJSON(query, function (data) {
-        console.log(currency.rate);
-        currency.rate = data.query.results.rate.Rate;
-     });
+    // var query = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22"+currency.convertFrom+currency.convertTo+"%22)&format=json&env=store://datatables.org/alltableswithkeys&callback=";
+    // $.getJSON(query, function (data) {
+    //     console.log(currency.rate);
+    //     currency.rate = data.query.results.rate.Rate;
+    //  });
         getCurrencyRate: function() {
             var query = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22"+currency.convertFrom+currency.convertTo+"%22)&format=json&env=store://datatables.org/alltableswithkeys&callback=";
              $.getJSON(query, function (data) {
