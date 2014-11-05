2b0c277b97df6729fac8c9e5ff6c2c61d65c8d46
Alex Griggs <alex@adventurebucketlist.com>
2014-11-04 18:49:31 -0800

    jQuery No Conflict

--- js/album.js
+++ js/album.js
@@ -234,7 +234,7 @@ app.controller('PaymentCtrl', function ($scope, $http, $timeout, productService,
             // process the form
          $scope.processPaymentForm = function(expr) {
                 var form = this;
-                $('#dropin').dispatchEvent(new Event('submit'));//tigger('submit');
+                jQuery('#dropin').dispatchEvent(new Event('submit'));//tigger('submit');
                 //$("#dropin")
                 $scope.selTime = $scope.timez[0];
                 form.formData.product_id = $scope.currentImage._id;
