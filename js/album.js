435fa73c1089286cf9493cb58aaf06bfbe3e4723
Alex Griggs <alex@adventurebucketlist.com>
2014-11-05 11:20:49 -0800

    changed around the braintree function back

--- js/album.js
+++ js/album.js
@@ -248,8 +248,8 @@ app.controller('PaymentCtrl', function ($scope, $http, $timeout, productService,
               cache: false,
               async: false
             });
-            
-        }
+        });
+
         var resultNonce ;
         setupBrainTree(function(nonce){
             // $.when.apply($, nonce).then(function() {
@@ -259,7 +259,7 @@ app.controller('PaymentCtrl', function ($scope, $http, $timeout, productService,
          });
          //resultNonce = nonce;
         //     $scope.nonce = nonce;
-        });
+       
                 // , "<integration>", options
             // create a blank object to hold our form information
             // $scope will allow this to pass between controller and view
